667e06974aabdfcacfd5e434575a9c5f2456c610
Farid Saleh <github.farid1@gmail.com>
2023-08-14 10:26:57 +0900

    update scripts

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,12 +8,13 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.shortcuts import render, redirect
 
 
 @login_required(login_url="/login/")
 def index(request):
+    return redirect("alihmedia_inactive_statistics")
     context = {'segment': 'index'}
-
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
 
