ccbfb8c84e5c9639253fe46c86dd4a578c10bf38
Farid Saleh <github.farid1@gmail.com>
2023-08-22 18:03:25 +0900

    update scripts

--- apps/alihmedia_utilities/views.py
+++ apps/alihmedia_utilities/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse, Http404
 from subprocess import Popen, check_call
 from django.contrib import messages
 from django.conf import settings
+from django.contrib.auth.decorators import permission_required, user_passes_test
+from django.contrib.auth.models import Group
 
 
 # Create your views here.
+@user_passes_test(lambda user: Group.objects.get(name='arsip') in user.groups.all())
 def inactive(request):
     comlist = [settings.PYTHON_UTILITY]
     if request.method == 'POST':
