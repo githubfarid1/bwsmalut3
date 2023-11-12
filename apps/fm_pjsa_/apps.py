9472ffe53735ee6bcf51c0fa656efcb3a9ba5a7f
githubfarid1 <github.farid1@gmail.com>
2023-11-12 11:23:51 +0900

    update scripts

--- apps/fm_pjsa_/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FmPjsaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.fm_pjsa'
--- apps/fm_pjsa_/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import atab, irwa_1, subfolder, tagged
-prefix = __package__.split('.')[1] + "_"
-urlpatterns = [
-    path(route='atab/<int:year>', view=atab, name=prefix + "atab"),
-    path(route='irwa_1/<int:year>', view=irwa_1, name=prefix + "irwa_1"),
-    path(route='subfolder/<int:id>', view=subfolder, name=prefix + "subfolder"),
-    path(route='tagged/<str:slug>', view=tagged, name=prefix + "tagged"),
-]
