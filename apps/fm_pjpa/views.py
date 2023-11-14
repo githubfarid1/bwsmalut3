99d99a9cb8af55973425c3bf5a77a0c1e70abccc
Muhammad Farid Saleh <github.farid1@gmail.com>
2023-11-14 15:37:36 +0900

    update scripts

--- apps/fm_pjpa/views.py
+++ apps/fm_pjpa/views.py
@@ -420,6 +420,7 @@ def build_breadcrumbs(url):
             'link': '/'.join(tmpl),
         }
         result.append(mdict)
+    result[:-1]['link'] = ''
     return result
 
 def showfolder(request, slug, year):
