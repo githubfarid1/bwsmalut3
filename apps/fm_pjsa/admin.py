d8d6a49749c77b0027dce86570b8feec42588d0b
Muhammad Farid Saleh <github.farid1@gmail.com>
2023-11-15 09:36:03 +0900

    update scripts

--- apps/fm_pjsa/admin.py
+++ apps/fm_pjsa/admin.py
@@ -2,5 +2,3 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 admin.site.register(models.Department)
-admin.site.register(models.Subfolder)
-admin.site.register(models.File)
